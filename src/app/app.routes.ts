5ba7d4c68fd3ad4af067ab1549b26de5f3ff4968
rewell namu <riwenamu@gmail.com>
2025-04-27 12:36:58 +0300

    more coding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'landing', pathMatch: 'full'},
+    {path: 'landing', loadChildren: () => import('./features/landing/landing.component').then(m => m.LandingComponent)},
+    {path: 'login', loadChildren: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)},
+    {path: 'signup', loadChildren: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent)},
+];
