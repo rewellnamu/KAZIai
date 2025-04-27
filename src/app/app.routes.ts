e79ba58584de90a955d1dadecaace2c9daa9d211
rewell namu <riwenamu@gmail.com>
2025-04-27 12:44:11 +0300

    format paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    {path: '', redirectTo: 'landing', pathMatch: 'full'},
-    {path: 'landing', loadChildren: () => import('./features/landing/landing.component').then(m => m.LandingComponent)},
-    {path: 'login', loadChildren: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)},
-    {path: 'signup', loadChildren: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent)}
-];
+export const routes: Routes = [];
