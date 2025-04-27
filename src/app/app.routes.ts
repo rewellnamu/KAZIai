3692b71501c59896131d9f64c36b6f593c9b8ba2
rewell namu <riwenamu@gmail.com>
2025-04-27 12:38:44 +0300

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ export const routes: Routes = [
     {path: '', redirectTo: 'landing', pathMatch: 'full'},
     {path: 'landing', loadChildren: () => import('./features/landing/landing.component').then(m => m.LandingComponent)},
     {path: 'login', loadChildren: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)},
-    {path: 'signup', loadChildren: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent)},
+    {path: 'signup', loadChildren: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent)}
 ];
